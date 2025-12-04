39ca40d87bd32c355941485f1df551afbbe785bc
ArturR0k3r <artur2003s.ass@gmail.com>
2025-12-04 21:14:14 +0200

    Placed increased wamr heap to psram fixed app start

--- samples/wasm_apps/hello_world/main.c
+++ samples/wasm_apps/hello_world/main.c
@@ -1,35 +1,29 @@
-/**
- * @file main.c
- * @brief Hello World - Simple AkiraOS WASM App Example
+/*
+ * @copyright Copyright © contributors to Project Ocre,
+ * which has been established as Project Ocre a Series of LF Projects, LLC
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Modified for AkiraOS - Hello World WASM App Example
  * 
  * This is a minimal example demonstrating how to create a WASM app
- * that runs on AkiraOS using WAMR libc-builtin.
- * 
- * Uses only libc-builtin functions (printf, puts) available in env module.
- * NO WASI functions - ESP32/WAMR runs in libc-builtin mode.
+ * that runs on AkiraOS using the Ocre Runtime.
  */
 
-/* Declare builtin functions that will be imported from "env" module */
-int printf(const char *fmt, ...);
-int puts(const char *str);
+#include <stdio.h>
 
-int main(void) 
-{  
-    puts("");
-    puts("========================================");
-    puts("    _    _    _           ___  ____    ");
-    puts("   / \\  | | _(_)_ __ __ _/ _ \\/ ___|   ");
-    puts("  / _ \\ | |/ / | '__/ _` | | | \\___ \\ ");
-    puts(" / ___ \\|   <| | | | (_| | |_| |___) |");
-    puts("/_/   \\_\\_|\\_\\_|_|  \\__,_|\\___/|____/ ");
-    puts("                                        ");
-    puts("    Hello from WASM App!                ");
-    puts("    Running on AkiraOS v1.2.1           ");
-    puts("    Powered by WebAssembly and OCRE     ");
-    puts("========================================");
-    puts("");
-    
-    printf("This message printed with printf!\n");
+// Application entry point
+int main()
+{
+    printf("\n\
+     _    _    _           ___  ____  \n\
+    / \\  | | _(_)_ __ __ _/ _ \\/ ___| \n\
+   / _ \\ | |/ / | '__/ _` | | | \\___ \\ \n\
+  / ___ \\|   <| | | | (_| | |_| |___) |\n\
+ /_/   \\_\\_|\\_\\_|_|  \\__,_|\\___/|____/ \n\
+                                       \n\
+      Hello from WASM App!             \n\
+      Running on AkiraOS v1.2.1        \n\
+                      powered by Ocre  \n");
     
     return 0;
 }
